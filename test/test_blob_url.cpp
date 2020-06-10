56e43c479aaa838d77aa27b4f06cf0ae40657636
janbernloehr <jan@bernloehrs.de>
2020-06-10 15:03:47 +0200

    Add support for unit tests

---
+++ test/test_blob_url.cpp
@@ -0,0 +1,32 @@
+
+#include "gtest/gtest.h"
+
+#include "rosbaz/blob_url.h"
+
+TEST(AzBlobUrl, can_parse_without_sas) {
+  const std::string url =
+      "https://contosoaccount.blob.core.windows.net/contosocontainer/my.bag";
+
+  const auto az_url = rosbaz::AzBlobUrl::parse(url);
+
+  EXPECT_EQ(az_url.schema, "https");
+  EXPECT_EQ(az_url.account_name, "contosoaccount");
+  EXPECT_EQ(az_url.blob_endpoint, "contosoaccount.blob.core.windows.net");
+  EXPECT_EQ(az_url.container_name, "contosocontainer");
+  EXPECT_EQ(az_url.blob_name, "my.bag");
+  EXPECT_EQ(az_url.sas_token, "");
+}
+
+TEST(AzBlobUrl, can_parse_with_sas) {
+  const std::string url = "https://contosoaccount.blob.core.windows.net/"
+                          "contosocontainer/my.bag?SAS_TOKEN";
+
+  const auto az_url = rosbaz::AzBlobUrl::parse(url);
+
+  EXPECT_EQ(az_url.schema, "https");
+  EXPECT_EQ(az_url.account_name, "contosoaccount");
+  EXPECT_EQ(az_url.blob_endpoint, "contosoaccount.blob.core.windows.net");
+  EXPECT_EQ(az_url.container_name, "contosocontainer");
+  EXPECT_EQ(az_url.blob_name, "my.bag");
+  EXPECT_EQ(az_url.sas_token, "?SAS_TOKEN");
+}
